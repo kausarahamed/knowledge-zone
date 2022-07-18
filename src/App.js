2e8dc3f24340f3825f76863cd8468198235815ee
faisalkn-71 <csust2021@gmail.com>
2022-07-19 00:00:15 +0600

    hello

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
     <div>
       <h1>Knowledge Zone</h1>
       <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi reprehenderit placeat fuga provident reiciendis laboriosam maxime architecto nobis, ea iste eos odit ab eius atque officiis sit, ipsam repellat. Harum?</h1>
-<<<<<<< HEAD
 
 
       <h1>Nipa's Branch</h1>
@@ -14,9 +13,7 @@ function App() {
       <h1>University</h1>
       
 
-=======
       <h1>saddam hossain</h1>
->>>>>>> 3443a3237ab01e09fc731be1f531ccf30157199f
     </div>
   );
 }
