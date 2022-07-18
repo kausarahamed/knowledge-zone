16cc009f413c059f4f5f2d119af341776a171272
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2022-07-18 23:50:33 +0600

    name send

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div>
       <h1>Knowledge Zone</h1>
+      <h2>hello all</h2>
       <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi reprehenderit placeat fuga provident reiciendis laboriosam maxime architecto nobis, ea iste eos odit ab eius atque officiis sit, ipsam repellat. Harum?</h1>
     </div>
   );
