bcfc16519fe0f4e46e4b736ce237af1a65fbc912
kausarahamed <galibhasan517@gmail.com>
2022-07-18 23:54:53 +0600

    daisy ui

--- src/App.js
+++ src/App.js
@@ -4,19 +4,22 @@ function App() {
   return (
     <div>
       <h1>Knowledge Zone</h1>
-      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi reprehenderit placeat fuga provident reiciendis laboriosam maxime architecto nobis, ea iste eos odit ab eius atque officiis sit, ipsam repellat. Harum?</h1>
-<<<<<<< HEAD
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+        reprehenderit placeat fuga provident reiciendis laboriosam maxime
+        architecto nobis, ea iste eos odit ab eius atque officiis sit, ipsam
+        repellat. Harum?
+      </h1>
 
-
+      <button class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
+        Button
+      </button>
       <h1>Nipa's Branch</h1>
       <h1>Faisal's Branch</h1>
-      
-      <h1>University</h1>
-      
 
-=======
+      <h1>University</h1>
+
       <h1>saddam hossain</h1>
->>>>>>> 3443a3237ab01e09fc731be1f531ccf30157199f
     </div>
   );
 }
