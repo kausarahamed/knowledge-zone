d7a77282cc7eb10af42e0c888064fe7ea9a19353
saddamhosan <contact.saddamhosan.com>
2022-07-19 00:28:27 +0600

    update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div>
-      <h1>Knowledge Zone</h1>
+      <h1>Knowledge ZONE</h1>
 
       <h1>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
