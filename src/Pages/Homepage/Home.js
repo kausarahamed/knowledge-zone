17edb9d2471f45ac98218cab20b3fce59ad69301
faisalkn-71 <csust2021@gmail.com>
2022-07-19 00:30:50 +0600

    reviews

--- src/Pages/Homepage/Home.js
+++ src/Pages/Homepage/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Reviews from "../../Reviews/Reviews";
 
 const Home = () => {
   return (
     <div>
       <h1>home</h1>
+      <Reviews></Reviews>
     </div>
   );
 };
